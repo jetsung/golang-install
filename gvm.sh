73ae674b74a687829e96c671132178648d94a77b
Jetsung Chan <i@jetsung.com>
2023-02-12 02:39:18 +0800

    feat: go version manager

---
+++ gvm.sh
@@ -0,0 +1,414 @@
+#!/usr/bin/env bash
+
+# Golang Version Manager
+# Project Home Page:
+# https://github.com/jetsung/golang-install
+# https://jihulab.com/jetsung/golang-install
+#
+# Author: Jetsung Chan <jetsungchan@gmail.com>
+
+# sh echo
+sh_echo() {
+    command printf %s\\n "$*" 2>/dev/null
+}
+
+# Get PROFILE
+detect_profile() {
+    if [ "${PROFILE-}" = '/dev/null' ]; then
+        # the user has specifically requested NOT to have nvm touch their profile
+        return
+    fi
+
+    if [ -n "${PROFILE}" ] && [ -f "${PROFILE}" ]; then
+        sh_echo "${PROFILE}"
+        return
+    fi
+
+    local DETECTED_PROFILE
+    DETECTED_PROFILE=''
+
+    if [ "${SHELL#*bash}" != "$SHELL" ]; then
+        if [ -f "$HOME/.bashrc" ]; then
+            DETECTED_PROFILE="$HOME/.bashrc"
+        elif [ -f "$HOME/.bash_profile" ]; then
+            DETECTED_PROFILE="$HOME/.bash_profile"
+        fi
+    elif [ "${SHELL#*zsh}" != "$SHELL" ]; then
+        if [ -f "$HOME/.zshrc" ]; then
+            DETECTED_PROFILE="$HOME/.zshrc"
+        fi
+    fi
+
+    if [ -z "$DETECTED_PROFILE" ]; then
+        for EACH_PROFILE in ".profile" ".bashrc" ".bash_profile" ".zshrc"; do
+            if DETECTED_PROFILE="$(try_profile "${HOME}/${EACH_PROFILE}")"; then
+                break
+            fi
+        done
+    fi
+
+    if [ -n "$DETECTED_PROFILE" ]; then
+        sh_echo "$DETECTED_PROFILE"
+    fi
+}
+
+init_os() {
+    OS=$(uname | tr '[:upper:]' '[:lower:]')
+    case "${OS}" in
+    darwin) OS='darwin' ;;
+    linux) OS='linux' ;;
+    freebsd) OS='freebsd' ;;
+    *)
+        printf "\e[1;31mOS %s is not supported by this installation script\e[0m\n" "${OS}"
+        exit 1
+        ;;
+    esac
+}
+
+sedi() {
+    if [ "${OS}" = "darwin" ]; then
+        sed -i "" "$@"
+    else
+        sed -i "$@"
+    fi
+}
+
+check_in_china() {
+    urlstatus=$(curl -s -m 3 -IL https://google.com | grep 200)
+    if [[ -z "${urlstatus}" ]]; then
+        IN_CHINA=1
+    fi
+}
+
+load_vars() {
+    GVM_VERSION="1.0.1"
+
+    GVM_PATH="${HOME}/.gvm"
+    GVM_BIN_PATH="${GVM_PATH}/bin"
+    GVM_ENV_PATH="${GVM_PATH}/env"
+    GVM_GO_ROOT="${GVM_PATH}/go"
+
+    GO_INSTALL_SCRIPT="${GVM_PATH}/install.sh"
+    GO_VERSIONS_PATH="${GVM_PATH}/verions"
+
+    PRO_URL="https://raw.githubusercontent.com/jetsung/golang-install/main/"
+    PRO_CN_URL="https://jihulab.com/jetsung/golang-install/-/raw/main/"
+
+    PROFILE="$(detect_profile)"
+
+    [[ -d "${GO_VERSIONS_PATH}" ]] || mkdir -p "${GO_VERSIONS_PATH}"
+}
+
+init_vars() {
+    local INDEX=0
+    for ARG in "$@"; do
+        ((INDEX++))
+        case "${INDEX}" in
+        1)
+            COMMAND="${ARG}"
+            ;;
+        2)
+            OPTIONS="${ARG}"
+            ;;
+        *)
+            EXT_ARGS[${#EXT_ARGS[@]}]="${ARG}"
+            ;;
+
+        esac
+    done
+}
+
+set_environment() {
+    tee "${GVM_ENV_PATH}" >/dev/null 2>&1 <<-EOF
+export GVMPATH="\$HOME/.gvm"
+export PATH="\$PATH:\$GVMPATH/bin"
+EOF
+
+    if ! grep -q "\$HOME/.gvm/env" "${PROFILE}"; then
+        printf "\n## GVM\n" >>"${PROFILE}"
+        echo ". \"\$HOME/.gvm/env\"" >>"${PROFILE}"
+    else
+        sedi "s@^. \"\$HOME/.gvm/.*@. \"\$HOME/.gvm/env\"@" "${PROFILE}"
+    fi
+}
+
+do_something() {
+    case "${COMMAND}" in
+    -h | --help)
+        show_help_message
+        ;;
+
+    -i | --install)
+        install_script
+        ;;
+
+    -u | --upgrade)
+        upgrade_script
+        ;;
+
+    -v | --version)
+        printf "gvm version: %s\n" "${GVM_VERSION}"
+        exit
+        ;;
+
+    current)
+        go version
+        exit
+        ;;
+
+    install)
+        if [[ -z "${OPTIONS}" ]]; then
+            printf "miss go version\n"
+            exit
+        fi
+        GO_VERSION="${OPTIONS}"
+        install_go
+        exit
+        ;;
+
+    list)
+        show_list
+        exit
+        ;;
+
+    list-remote)
+        show_remote_list
+        exit
+        ;;
+
+    uninstall)
+        if [[ -z "${OPTIONS}" ]]; then
+            printf "miss go version\n"
+            exit
+        fi
+        GO_VERSION="${OPTIONS}"
+        uninstall_go
+        exit
+        ;;
+
+    use)
+        if [[ -z "${OPTIONS}" ]]; then
+            printf "miss go version\n"
+            exit
+        fi
+        GO_VERSION="${OPTIONS}"
+        use_go
+        exit
+        ;;
+    esac
+}
+
+# show help message
+show_help_message() {
+    printf "\e[1;32mgvm\e[m %s
+Golang Version Manager
+
+\e[1;33mUSAGE:\e[m
+    %s [OPTIONS] <SUBCOMMANDS>
+
+\e[1;33mOPTIONS:\e[m
+    \e[1;32m-h, --help\e[m
+                Print help information
+            
+    \e[1;32m-i, --install\e[m
+                Install Golang Version Manager
+
+    \e[1;32m-v, --version\e[m
+                Print Gvm version information
+
+\e[1;33mSUBCOMMANDS:\e[m
+  \e[1;32mcurrent\e[m    
+                Print the current go version
+  \e[1;32minstall\e[m    
+                Install a new go version  
+  \e[1;32mlist\e[m       
+                List all locally installed go versions
+  \e[1;32mlist-remote\e[m
+                List all remote go versions <more>
+  \e[1;32muninstall\e[m        
+                Uninstall a Go version                
+  \e[1;32muse\e[m        
+                Change Go version
+\n" "${GVM_VERSION}" "${SCRIPT_NAME##*/}"
+    exit 1
+}
+
+dl_goinstall_script() {
+    [[ -z "${1}" ]] || rm -rf "${GO_INSTALL_SCRIPT}"
+
+    if [[ ! -f "${GO_INSTALL_SCRIPT}" ]]; then
+        if [[ -f "./install.sh" ]]; then
+            cp -f "./install.sh" "${GO_INSTALL_SCRIPT}"
+        else
+            check_in_china
+            [[ -z "${IN_CHINA}" ]] || PRO_URL="${PRO_CN_URL}"
+            wget -q -O "${GO_INSTALL_SCRIPT}" "${PRO_URL}/install.sh"
+        fi
+    fi
+    if [[ ! -f "${GO_INSTALL_SCRIPT}" ]]; then
+        printf "\e[1;31mGVM go-install.sh download failed\e[m\n"
+        exit
+    fi
+    chmod +x "${GO_INSTALL_SCRIPT}"
+}
+
+gvm_script() {
+    [[ -d "${GVM_BIN_PATH}" ]] || mkdir -p "${GVM_BIN_PATH}"
+
+    DIR_PATH=$(dirname "$0")
+    SCRIPT_PATH=$(
+        cd "${DIR_PATH}" || exit
+        pwd
+    )
+
+    if [[ "${GVM_BIN_PATH}" == "${SCRIPT_PATH}" ]]; then
+        printf "\e[1;31mGVM(%s) is the same file\e[m\n" "${SCRIPT_NAME}"
+        exit
+    fi
+
+    local GVM_SCRIPT_PATH="${GVM_BIN_PATH}/gvm.sh"
+    if [[ -f "${SCRIPT_NAME}" ]]; then
+        cp "${SCRIPT_NAME}" "${GVM_BIN_PATH}"
+    else
+        check_in_china
+        [[ -z "${IN_CHINA}" ]] || PRO_URL="${PRO_CN_URL}"
+        wget -q -O "${GVM_SCRIPT_PATH}" "${PRO_URL}/gvm.sh"
+    fi
+
+    if [[ ! -f "${GVM_SCRIPT_PATH}" ]]; then
+        printf "\e[1;31mGVM(gvm.sh) download failed\e[m\n"
+    fi
+    chmod +x "${GVM_SCRIPT_PATH}"
+}
+
+install_script() {
+    gvm_script
+    dl_goinstall_script ""
+
+    printf "GVM successfully installed\n"
+    exit
+}
+
+upgrade_script() {
+    gvm_script
+    dl_goinstall_script "upgrade"
+
+    printf "GVM successfully updated\n"
+    exit
+}
+
+go_list() {
+    # GO_VERSION_LIST=$(find "${GO_VERSIONS_PATH}" -maxdepth 1 -name "go*" -type d | cut -d '/' -f 6 | sed 's/..//')
+    while IFS= read -r -d '' _V; do
+        GO_VERSION_LIST[${#GO_VERSION_LIST[@]}]=$(${_V} version | awk '{print $3}' | sed 's/..//')
+    done < <(find "${GO_VERSIONS_PATH}"/go*/bin/go -maxdepth 3 -type f -print0)
+}
+
+go_list_remote() {
+    check_in_china
+    local GO_DL_URL="https://go.dev/dl/"
+    [[ -z "${IN_CHINA}" ]] || GO_DL_URL="https://golang.google.cn/dl/"
+
+    RELEASE_TAGS="$(curl -sL --retry 5 "${GO_DL_URL}" | sed -n '/toggle/p' | cut -d '"' -f 4 | grep go)"
+
+    while IFS=$'\n' read -r -d ' ' _V; do
+        REMOTE_GO_LIST[${#REMOTE_GO_LIST[@]}]="${_V}"
+    done < <(echo "${RELEASE_TAGS}" | tr -s '\n' ' ')
+}
+
+show_list() {
+    go_list
+
+    for _V in "${GO_VERSION_LIST[@]}"; do
+        printf "go%s\n" "${_V}"
+    done
+}
+
+show_remote_list() {
+    go_list_remote
+
+    if [[ "${OPTIONS}" == "more" ]]; then
+        SHOW_ALL="more"
+    fi
+
+    local INDEX=0
+    for _V in "${REMOTE_GO_LIST[@]}"; do
+        ((INDEX++))
+
+        if [[ ${INDEX} -gt 10 ]] && [[ -z "${SHOW_ALL}" ]]; then
+            break
+        fi
+
+        printf "%s\n" "${_V}"
+    done
+}
+
+use_go() {
+    CURRENT_GO_BINARY="${GO_VERSIONS_PATH}/go${GO_VERSION}/bin/go"
+    if [[ ! -f "${CURRENT_GO_BINARY}" ]]; then
+        install_go
+    else
+        ${CURRENT_GO_BINARY} version
+    fi
+
+    if [[ -f "${CURRENT_GO_BINARY}" ]]; then
+        rm -rf "${GVM_GO_ROOT}"
+        ln -s "${GO_VERSIONS_PATH}/go${GO_VERSION}" "${GVM_GO_ROOT}"
+        sedi "s@^export GOROOT.*@export GOROOT=\"${GVM_GO_ROOT}\"@" "${PROFILE}"
+
+        # sedi "s@^export GOROOT.*@export GOROOT=\"${GO_VERSIONS_PATH}/go${GO_VERSION}\"@" "${PROFILE}"
+        printf "\nYou need to execute: \n\e[1;33msource %s\e[m\n" "${PROFILE}"
+    fi
+}
+
+uninstall_go() {
+    if [[ $(go version | awk '{print $3}') == "go${GO_VERSION}" ]]; then
+        printf "\e[1;31mThe current version(go%s) is in use\e[m\n" "${GO_VERSION}"
+        exit
+    fi
+
+    if [[ -d "${GO_VERSIONS_PATH}/go${GO_VERSION}" ]]; then
+        rm -rf "${GO_VERSIONS_PATH}/go${GO_VERSION}"
+        printf "go%s uninstalled successfully\n" "${GO_VERSION}"
+    fi
+}
+
+install_go() {
+    go_list
+
+    for _V in "${GO_VERSION_LIST[@]}"; do
+        if [[ "${_V}" == "${GO_VERSION}" ]]; then
+            printf "\e[1;31mGo %s already exists\e[m\n" "${GO_VERSION}"
+            exit
+        fi
+    done
+
+    printf "\e[1;33mInstalling go %s\e[m\n" "${GO_VERSION}"
+
+    PARAMS="-v ${GO_VERSION} -c ${GO_VERSIONS_PATH}/go${GO_VERSION}"
+    # echo "${GO_INSTALL_SCRIPT} ${PARAMS}"
+
+    # shellcheck disable=SC2086
+    ${SHELL} ${GO_INSTALL_SCRIPT} ${PARAMS} >"${HOME}/.gvm.log" 2>&1
+    # shellcheck disable=SC1090,SC2086
+    CURRENT_GO_BINARY="${GO_VERSIONS_PATH}/go${GO_VERSION}/bin/go"
+    if [[ -f "${CURRENT_GO_BINARY}" ]]; then
+        ${CURRENT_GO_BINARY} version
+        sedi "s@^export GOROOT.*@export GOROOT=\"${GVM_GO_ROOT}\"@" "${PROFILE}"
+    else
+        printf "\e[1;31mGo %s installation failed\e[m\n" "${GO_VERSION}"
+    fi
+}
+
+main() {
+    SCRIPT_NAME="$0"
+    init_os
+    load_vars
+    init_vars "$@"
+
+    set_environment
+
+    do_something
+}
+
+main "$@" || exit 1
--- install.sh
+++ install.sh
@@ -35,17 +35,17 @@ load_vars() {
 # check in china
 check_in_china() {
     urlstatus=$(curl -s -m 3 -IL https://google.com | grep 200)
-    if [ "${urlstatus}" == "" ]; then
+    if [[ -z "${urlstatus}" ]]; then
         IN_CHINA=1
         RELEASE_URL="https://golang.google.cn/dl/"
         PROJECT_URL="https://jihulab.com/jetsung/golang-install"
-        GOPROXY_TEXT="https://goproxy.cn,https://goproxy.io"   
+        GOPROXY_TEXT="https://goproxy.cn,https://goproxy.io"
     fi
 }
 
 # custom version
 custom_version() {
-    if [ -n "${1}" ] ;then
+    if [[ -n "${1}" ]]; then
         RELEASE_TAG="go${1}"
         echo "Custom Version = ${RELEASE_TAG}"
     fi
@@ -53,12 +53,12 @@ custom_version() {
 
 # create GOPATH folder
 create_gopath() {
-    if [ ! -d ${GO_PATH} ]; then
-        if [ "${GO_PATH}" = "~/go" ] || [ "${GO_PATH}" = "\$HOME/go" ]; then
-            mkdir -p ${HOME}/go
+    if [[ ! -d "${GO_PATH}" ]]; then
+        if [[ "${GO_PATH}" == "\$HOME/go" ]]; then
+            mkdir -p "${HOME}"/go
             GO_PATH="\$HOME/go"
         else
-            mkdir -p ${GO_PATH}
+            mkdir -p "${GO_PATH}"
         fi
     fi
 }
@@ -66,62 +66,72 @@ create_gopath() {
 # Get OS bit
 init_arch() {
     ARCH=$(uname -m)
-    BIT=$ARCH
-    case $ARCH in
-        amd64) ARCH="amd64";;
-        x86_64) ARCH="amd64";;
-        i386) ARCH="386";;
-        armv6l) ARCH="armv6l";; 
-        armv7l) ARCH="armv6l";; 
-        aarch64) ARCH="arm64";; 
-        *) printf "\e[1;31mArchitecture %s is not supported by this installation script\e[0m\n" $ARCH; exit 1;;
+    BIT="${ARCH}"
+
+    case "${ARCH}" in
+    amd64) ARCH="amd64" ;;
+    x86_64) ARCH="amd64" ;;
+    i386) ARCH="386" ;;
+    armv6l) ARCH="armv6l" ;;
+    armv7l) ARCH="armv6l" ;;
+    aarch64) ARCH="arm64" ;;
+    *)
+        printf "\e[1;31mArchitecture %s is not supported by this installation script\e[0m\n" "${ARCH}"
+        exit 1
+        ;;
     esac
-#    echo "ARCH = ${ARCH}"
+    #    echo "ARCH = ${ARCH}"
 }
 
 # Get OS version
 init_os() {
     OS=$(uname | tr '[:upper:]' '[:lower:]')
-    case $OS in
-        darwin) OS='darwin';;
-        linux) OS='linux';;
-        freebsd) OS='freebsd';;
-#        mingw*) OS='windows';;
-#        msys*) OS='windows';;
-        *) printf "\e[1;31mOS %s is not supported by this installation script\e[0m\n" $OS; exit 1;;
+    case "${OS}" in
+    darwin) OS='darwin' ;;
+    linux) OS='linux' ;;
+    freebsd) OS='freebsd' ;;
+        #        mingw*) OS='windows';;
+        #        msys*) OS='windows';;
+    *)
+        printf "\e[1;31mOS %s is not supported by this installation script\e[0m\n" "${OS}"
+        exit 1
+        ;;
     esac
-#    echo "OS = ${OS}"
+    #    echo "OS = ${OS}"
 }
 
 # init args
 init_args() {
-    key=""
-
-    for arg in "$@" 
-    do
-        if test "-h" = $arg; then
+    KEY=""
+    for ARG in "$@"; do
+        if [[ "-h" == "${ARG}" ]]; then
             show_help_message
         fi
 
-        if test -z $key; then 
-            key=$arg
-        else 
-            if test "-v" = $key; then
-                custom_version $arg
-            elif test "-d" = $key; then
-                GO_PATH=$arg
-            fi
-
-            key=""
+        if [[ -z "${KEY}" ]]; then
+            KEY="${ARG}"
+        else
+            case "${KEY}" in
+            "-v")
+                custom_version "$ARG"
+                ;;
+            "-d")
+                GO_PATH="${ARG}"
+                ;;
+            "-c")
+                GO_CUSTOM_PATH="${ARG}"
+                ;;
+            esac
+            KEY=""
         fi
     done
 }
 
 # if RELEASE_TAG was not provided, assume latest
 latest_version() {
-    if [ -z "${RELEASE_TAG}" ]; then
-        RELEASE_TAG="$(curl -sL --retry 5 ${RELEASE_URL} | sed -n '/toggleVisible/p' | head -n 1 | cut -d '"' -f 4)"
-#        echo "Latest Version = ${RELEASE_TAG}"
+    if [[ -z "${RELEASE_TAG}" ]]; then
+        RELEASE_TAG="$(curl -sL --retry 5 "${RELEASE_URL}" | sed -n '/toggleVisible/p' | head -n 1 | cut -d '"' -f 4)"
+        #        echo "Latest Version = ${RELEASE_TAG}"
     fi
 }
 
@@ -129,124 +139,139 @@ latest_version() {
 compare_version() {
     OLD_VERSION="none"
     NEW_VERSION="${RELEASE_TAG}"
-    if test -x "$(command -v go)"; then
-        OLD_VERSION="$(go version | awk '{print $3}')"
+    # if test -x "$(command -v go)"; then
+    #     OLD_VERSION="$(go version | awk '{print $3}')"
+    # fi
+    if [[ -z "${GO_CUSTOM_PATH}" ]]; then
+        if [[ -f "${HOME}/.go/bin/go" ]]; then
+            OLD_VERSION="$("${HOME}"/.go/bin/go version | awk '{print $3}')"
+        fi
+    else
+        if [[ -f "${GO_CUSTOM_PATH}/.go/bin/go" ]]; then
+            OLD_VERSION="$("${GO_CUSTOM_PATH}"/.go/bin/go version | awk '{print $3}')"
+        fi
     fi
-    if [ "$OLD_VERSION" = "$NEW_VERSION" ]; then
-       printf "\n\e[1;31mYou have installed this version: %s\e[0m\n" $OLD_VERSION; exit 1;
+    if [[ "${OLD_VERSION}" == "${NEW_VERSION}" ]]; then
+        printf "\n\e[1;31mYou have installed this version: %s\e[0m\n" "${OLD_VERSION}"
+        exit 1
     fi
 
-printf "
+    printf "
 Current version: \e[1;33m %s \e[0m 
 Target version: \e[1;33m %s \e[0m
-" $OLD_VERSION $NEW_VERSION
+" "$OLD_VERSION" "$NEW_VERSION"
 }
 
-# compare version size 
-version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
+# compare version size
+version_ge() { [[ "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1" ]]; }
 
 # install curl command
 install_curl_command() {
-    if !(test -x "$(command -v curl)"); then
+    if ! test -x "$(command -v curl)"; then
         if test -x "$(command -v yum)"; then
             yum install -y curl
         elif test -x "$(command -v apt)"; then
             apt install -y curl
-        else 
+        else
             printf "\e[1;31mYou must pre-install the curl tool\e[0m\n"
             exit 1
         fi
-    fi  
+    fi
 }
 
 # Download go file
 ## unused
 download_file() {
-    url="${1}"
+    URL="${1}"
     destination="${2}"
 
-    printf "Fetching ${url} \n\n"
+    printf "Fetching %s \n\n" "${URL}"
 
     if test -x "$(command -v curl)"; then
-        code=$(curl --connect-timeout 15 -w '%{http_code}' -L "${url}" -o "${destination}")
+        code=$(curl --connect-timeout 15 -w '%{http_code}' -L "${URL}" -o "${destination}")
     elif test -x "$(command -v wget)"; then
-        code=$(wget -t2 -T15 -O "${destination}" --server-response "${url}" 2>&1 | awk '/^  HTTP/{print $2}' | tail -1)
+        code=$(wget -t2 -T15 -O "${destination}" --server-response "${URL}" 2>&1 | awk '/^  HTTP/{print $2}' | tail -1)
     else
         printf "\e[1;31mNeither curl nor wget was available to perform http requests.\e[0m\n"
         exit 1
     fi
 
     if [ "${code}" != 200 ]; then
-        printf "\e[1;31mRequest failed with code %s\e[0m\n" $code
+        printf "\e[1;31mRequest failed with code %s\e[0m\n" "$code"
         exit 1
-    else 
-	    printf "\n\e[1;33mDownload succeeded\e[0m\n"
+    else
+        printf "\n\e[1;33mDownload succeeded\e[0m\n"
     fi
 }
 
 # Download file and unpack
 download_unpack() {
-    printf "Fetching ${1} \n\n"
+    printf "Fetching %s \n\n" "${1}"
 
-    rm -rf ${HOME}/.go
-
-    curl -Lk --retry 3 "${1}" | gunzip | tar xf - -C /tmp
-
-    mv /tmp/go ${HOME}/.go
+    if [[ -z "${GO_CUSTOM_PATH}" ]]; then
+        rm -rf "${HOME}"/.go
+        curl -Lk --retry 3 "${1}" | gunzip | tar xf - -C /tmp
+        mv /tmp/go "${HOME}"/.go
+    else
+        rm -rf "${GO_CUSTOM_PATH}"
+        PARENT_PATH=$(dirname "${GO_CUSTOM_PATH}")
+        [[ -d "${PARENT_PATH}" ]] || mkdir -p "${PARENT_PATH}"
+        curl -Lk --retry 3 "${1}" | gunzip | tar xf - -C /tmp
+        mv /tmp/go "${GO_CUSTOM_PATH}"
+    fi
 }
 
 # Get PROFILE
 detect_profile() {
-  if [ "${PROFILE-}" = '/dev/null' ]; then
-    # the user has specifically requested NOT to have nvm touch their profile
-    return
-  fi
-
-  if [ -n "${PROFILE}" ] && [ -f "${PROFILE}" ]; then
-    sh_echo "${PROFILE}"
-    return
-  fi
-
-  local DETECTED_PROFILE
-  DETECTED_PROFILE=''
-
-  if [ "${SHELL#*bash}" != "$SHELL" ]; then
-    if [ -f "$HOME/.bashrc" ]; then
-      DETECTED_PROFILE="$HOME/.bashrc"
-    elif [ -f "$HOME/.bash_profile" ]; then
-      DETECTED_PROFILE="$HOME/.bash_profile"
+    if [ "${PROFILE-}" = '/dev/null' ]; then
+        # the user has specifically requested NOT to have nvm touch their profile
+        return
     fi
-  elif [ "${SHELL#*zsh}" != "$SHELL" ]; then
-    if [ -f "$HOME/.zshrc" ]; then
-      DETECTED_PROFILE="$HOME/.zshrc"
+
+    if [ -n "${PROFILE}" ] && [ -f "${PROFILE}" ]; then
+        sh_echo "${PROFILE}"
+        return
     fi
-  fi
 
-  if [ -z "$DETECTED_PROFILE" ]; then
-    for EACH_PROFILE in ".profile" ".bashrc" ".bash_profile" ".zshrc"
-    do
-      if DETECTED_PROFILE="$(try_profile "${HOME}/${EACH_PROFILE}")"; then
-        break
-      fi
-    done
-  fi
+    local DETECTED_PROFILE
+    DETECTED_PROFILE=''
 
-  if [ -n "$DETECTED_PROFILE" ]; then
-    sh_echo "$DETECTED_PROFILE"
-  fi
+    if [ "${SHELL#*bash}" != "$SHELL" ]; then
+        if [ -f "$HOME/.bashrc" ]; then
+            DETECTED_PROFILE="$HOME/.bashrc"
+        elif [ -f "$HOME/.bash_profile" ]; then
+            DETECTED_PROFILE="$HOME/.bash_profile"
+        fi
+    elif [ "${SHELL#*zsh}" != "$SHELL" ]; then
+        if [ -f "$HOME/.zshrc" ]; then
+            DETECTED_PROFILE="$HOME/.zshrc"
+        fi
+    fi
+
+    if [ -z "$DETECTED_PROFILE" ]; then
+        for EACH_PROFILE in ".profile" ".bashrc" ".bash_profile" ".zshrc"; do
+            if DETECTED_PROFILE="$(try_profile "${HOME}/${EACH_PROFILE}")"; then
+                break
+            fi
+        done
+    fi
+
+    if [ -n "$DETECTED_PROFILE" ]; then
+        sh_echo "$DETECTED_PROFILE"
+    fi
 }
 
 # try profile
 try_profile() {
-  if [ -z "${1-}" ] || [ ! -f "${1}" ]; then
-    return 1
-  fi
-  sh_echo "${1}"
+    if [ -z "${1-}" ] || [ ! -f "${1}" ]; then
+        return 1
+    fi
+    sh_echo "${1}"
 }
 
 # sh echo
 sh_echo() {
-  command printf %s\\n "$*" 2>/dev/null
+    command printf %s\\n "$*" 2>/dev/null
 }
 
 # fix macos
@@ -260,57 +285,61 @@ sedi() {
 
 # set golang environment
 set_environment() {
-    if [ -z "`grep 'export\sGOROOT' ${PROFILE}`" ];then
-        printf "\n## GOLANG\n" >> "${PROFILE}"
-        echo "export GOROOT=\"\$HOME/.go\"" >> "${PROFILE}"
+    if ! grep -q 'export\sGOROOT' "${PROFILE}"; then
+        printf "\n## GOLANG\n" >>"${PROFILE}"
+        echo "export GOROOT=\"\$HOME/.go\"" >>"${PROFILE}"
     else
         sedi "s@^export GOROOT.*@export GOROOT=\"\$HOME/.go\"@" "${PROFILE}"
     fi
 
-    if [ -z "`grep 'export\sGOPATH' ${PROFILE}`" ];then
-        echo "export GOPATH=\"${GO_PATH}\"" >> "${PROFILE}"
+    if ! grep -q 'export\sGOPATH' "${PROFILE}"; then
+        echo "export GOPATH=\"${GO_PATH}\"" >>"${PROFILE}"
     else
         sedi "s@^export GOPATH.*@export GOPATH=\"${GO_PATH}\"@" "${PROFILE}"
     fi
-    
-    if [ -z "`grep 'export\sGOBIN' ${PROFILE}`" ];then
-        echo "export GOBIN=\"\$GOPATH/bin\"" >> ${PROFILE}
-    else 
-        sedi "s@^export GOBIN.*@export GOBIN=\$GOPATH/bin@" "${PROFILE}"     
-    fi   
 
-    if [ -z "`grep 'export\sGO111MODULE' ${PROFILE}`" ];then
-        echo "export GO111MODULE=on" >> "${PROFILE}"
-    fi       
+    if ! grep -q 'export\sGOBIN' "${PROFILE}"; then
+        echo "export GOBIN=\"\$GOPATH/bin\"" >>"${PROFILE}"
+    else
+        sedi "s@^export GOBIN.*@export GOBIN=\$GOPATH/bin@" "${PROFILE}"
+    fi
 
-    if [ -z "`grep 'export\sASSUME_NO_MOVING_GC_UNSAFE_RISK_IT_WITH' ${PROFILE}`" ];then
+    if ! grep -q 'export\sGO111MODULE' "${PROFILE}"; then
+        echo "export GO111MODULE=on" >>"${PROFILE}"
+    fi
+
+    if ! grep -q 'export\sASSUME_NO_MOVING_GC_UNSAFE_RISK_IT_WITH' "${PROFILE}"; then
         if version_ge "${RELEASE_TAG}" "go1.17"; then
-            echo "export ASSUME_NO_MOVING_GC_UNSAFE_RISK_IT_WITH=go1.18" >> "${PROFILE}"
+            echo "export ASSUME_NO_MOVING_GC_UNSAFE_RISK_IT_WITH=go1.18" >>"${PROFILE}"
         fi
     fi
 
-    if [ "${IN_CHINA}" == "1" ]; then 
-        if [ -z "`grep 'export\sGOSUMDB' ${PROFILE}`" ];then
-            echo "export GOSUMDB=off" >> "${PROFILE}"
-        fi      
+    if [[ -n "${IN_CHINA}" ]]; then
+        if ! grep -q 'export\sGOSUMDB' "${PROFILE}"; then
+            echo "export GOSUMDB=off" >>"${PROFILE}"
+        fi
     fi
 
-    if [ -z "`grep 'export\sGOPROXY' ${PROFILE}`" ];then
-        echo "export GOPROXY=\"${GOPROXY_TEXT},direct\"" >> "${PROFILE}"
-    else 
-        sedi "s@^export GOPROXY.*@export GOPROXY=\"${GOPROXY_TEXT},direct\"@" "${PROFILE}"     
-    fi 
-    
-    if [ -z "`grep '\$GOROOT/bin:\$GOBIN' ${PROFILE}`" ];then
-        echo "export PATH=\"\$PATH:\$GOROOT/bin:\$GOBIN\"" >> "${PROFILE}"
-    fi        
+    if ! grep -q 'export\sGOPROXY' "${PROFILE}"; then
+        echo "export GOPROXY=\"${GOPROXY_TEXT},direct\"" >>"${PROFILE}"
+    else
+        sedi "s@^export GOPROXY.*@export GOPROXY=\"${GOPROXY_TEXT},direct\"@" "${PROFILE}"
+    fi
+
+    if ! grep -q "\$GOROOT/bin:\$GOBIN" "${PROFILE}"; then
+        echo "export PATH=\"\$PATH:\$GOROOT/bin:\$GOBIN\"" >>"${PROFILE}"
+    fi
+
+    if [[ -n "${GO_CUSTOM_PATH}" ]]; then
+        sedi "s@^export GOROOT.*@export GOROOT=\"${GO_CUSTOM_PATH}\"@" "${PROFILE}"
+    fi
 }
 
 # show copyright
 show_copyright() {
     clear
 
-printf "
+    printf "
 ###############################################################
 ###  Golang Install
 ###
@@ -323,7 +352,7 @@ printf "
 
 # show system information
 show_system_information() {
-printf "
+    printf "
 ###############################################################
 ###  System: %s 
 ###  Bit: %s 
@@ -334,7 +363,7 @@ printf "
 
 # Show success message
 show_success_message() {
-printf "
+    printf "
 ###############################################################
 # Install success, please execute again \e[1;33msource %s\e[0m
 ###############################################################
@@ -343,8 +372,7 @@ printf "
 
 # show help message
 show_help_message() {
-printf "
-Go install
+    printf "Go install
 
 Usage: %s [-h] [-v version] [-d GOPATH]
 
@@ -353,11 +381,11 @@ Options:
   -v            : set go version (default: latest version)
   -d            : set GOPATH (default: \$HOME/go)
 \n" "${SCRIPT_NAME}"
-exit 1
+    exit 1
 }
 
 main() {
-    load_vars "$@"
+    load_vars "$0"
 
     init_args "$@"
 
@@ -387,10 +415,10 @@ main() {
 
     # Download and unpack
     download_unpack "${BINARY_URL}"
-    
+
     # Set ENV
     set_environment
-    
+
     show_success_message
 }
 
